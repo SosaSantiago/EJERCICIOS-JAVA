5995b67f77a4b6bccb6ff8d109a031a9b80b6951
Azz <santiagososa20006@gmail.com>
2026-09-06 20:24:50 -0300

    Delete UNIDAD-2/EJERCITACION 2.1/Main.java

--- UNIDAD-2/EJERCITACION 2.1/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        persona persona1 = new persona("Matias", 21);
-        System.out.println("Nombre: " + persona1.getNombre());
-        System.out.println("Edad: " + persona1.getEdad());
-    }
-}
