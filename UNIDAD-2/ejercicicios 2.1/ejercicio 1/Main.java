271a0bdb9eaa6020bf9f947465959b09933b44ec
Azz <santiagososa20006@gmail.com>
2026-09-06 20:27:50 -0300

    Add files via upload

---
+++ UNIDAD-2/ejercicicios 2.1/ejercicio 1/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        persona persona1 = new persona("Matias", 21);
+        System.out.println("Nombre: " + persona1.getNombre());
+        System.out.println("Edad: " + persona1.getEdad());
+    }
+}
